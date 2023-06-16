e919ea148e33df2e596bf266a8d67070f746a816
raksha0000 <rakshithashetty0904@gmail.com>
2023-06-16 18:51:55 +0530

    updated

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,144 +1,117 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
 import RadioButtonComponent from './RadioButtonComponent';
-import ConnectComponent from './ConnectComponent';
-import CheckboxComponent from './CheckboxComponent';
-import TopicSelectionComponent from './TopicSelectionComponent';
-import MentorTypeComponent from './MentorTypeComponent';
-import ProfileComponent from './ProfileComponent';
-import CareerOptionsComponent from './CareerOptionsComponent';
-import MentoringCall from './MentoringCall';
-import ReviewComponent from './ReviewComponent';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 const LoginPage = () => {
-    const [login1Data, setLogin1Data] = useState({
-        username: '',
-        password: '',
-    });
+  const [activeLogin, setActiveLogin] = useState(null);
+  const [mentorLogin, setMentorLogin] = useState(false);
+  const [gmailLogin, setGmailLogin] = useState(false);
+  const [showRadioButton, setShowRadioButton] = useState(false);
+  const [previousPage, setPreviousPage] = useState(null);
 
-    const [login2Data, setLogin2Data] = useState({
-        username: '',
-        password: '',
-    });
+  const handleLoginClick = () => {
+    setPreviousPage(activeLogin);
+    setActiveLogin(2);
+  };
 
-    const [activeLogin, setActiveLogin] = useState(null);
+  const handleMentorLoginClick = () => {
+    setPreviousPage(activeLogin);
+    setMentorLogin(true);
+    setGmailLogin(false);
+  };
 
-    const handleLoginClick = (loginNumber) => {
-        setActiveLogin(loginNumber);
-    };
+  const handleGmailLoginClick = () => {
+    setPreviousPage(activeLogin);
+    setGmailLogin(true);
+    setMentorLogin(false);
+  };
 
-    const handleLoginSubmit = (event) => {
-        event.preventDefault();
-        if (activeLogin === 1) {
-            console.log('Login 1 data:', login1Data);
-            setLogin1Data({ username: '', password: '' });
-        } else if (activeLogin === 2) {
-            console.log('Login 2 data:', login2Data);
-            setLogin2Data({ username: '', password: '' });
-        }
-        setActiveLogin(null);
-    };
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+    console.log('Login submitted');
+    setPreviousPage(activeLogin);
+    setActiveLogin(null);
+    setMentorLogin(false);
+    setGmailLogin(false);
+    setShowRadioButton(true);
+  };
 
-    const handleLoginChange = (event) => {
-        const { name, value } = event.target;
-        if (activeLogin === 1) {
-            setLogin1Data((prevState) => ({
-                ...prevState,
-                [name]: value,
-            }));
-        } else if (activeLogin === 2) {
-            setLogin2Data((prevState) => ({
-                ...prevState,
-                [name]: value,
-            }));
-        }
-    };
+  const handleGoBack = () => {
+    if (gmailLogin) {
+      setGmailLogin(false);
+      setMentorLogin(true);
+    } else if (mentorLogin) {
+      setMentorLogin(false);
+      setActiveLogin(previousPage);
+    } else if (showRadioButton) {
+      setShowRadioButton(false);
+    }
+  };
 
-    return (
-        <div className="container">
-            <div className="outer-box">
-                <div className="login-box"></div>
-                <div className="login-page">
-                    <div className="login-box">
-                        <h2>Welcome to CoCo Mentors</h2>
-                        <div className="login-buttons">
-                            <button className={`login-button ${activeLogin === 1 ? 'active' : ''}`} onClick={() => handleLoginClick(1)}>Do You Want To Mentor?</button>
-                            <button className={`login-button ${activeLogin === 2 ? 'active' : ''}`} onClick={() => handleLoginClick(2)}>Do You Want To Get Mentored?</button>
-                        </div>
-                        {activeLogin && (
-                            <form onSubmit={handleLoginSubmit}>
-                                <input
-                                    type="text"
-                                    name="username"
-                                    placeholder="Username"
-                                    value={activeLogin === 1 ? login1Data.username : login2Data.username}
-                                    onChange={handleLoginChange}
-                                />
-                                <input
-                                    type="password"
-                                    name="password"
-                                    placeholder="Password"
-                                    value={activeLogin === 1 ? login1Data.password : login2Data.password}
-                                    onChange={handleLoginChange}
-                                />
-                                <button type="submit">Submit</button>
-                            </form>
-                        )}
-                        {activeLogin && (
-                            <div>
-                                <p>Register for new user</p>
-                                {/* Add your registration form or link here */}
-                            </div>
-                        )}
-                    </div>
-                </div>
+  return (
+    <div className="container">
+      {activeLogin === null && !showRadioButton && (
+        <div className="outer-box">
+          <div className="login-box">
+            <h2>Welcome to CoCo Mentors</h2>
+            <div className="login-buttons">
+              <button className="login-button" onClick={handleLoginClick}>
+                Do You Want To Mentor?
+              </button>
+              <button className="login-button">Do You Want To Get Mentored?</button>
             </div>
-
-            <div className="second-login-box">
-                <div className="login-fields">
-                    <button
-                        className="login-field"
-                        onClick={() => console.log('Clicked on Login Field 1')}
-                    >
-                        Login as a Mentor
-                    </button>
-                    <button
-                        className="login-field"
-                        onClick={() => console.log('Clicked on Login Field 2')}
-                    >
-                        Login with Gmail
-                    </button>
-                </div>
-            </div>
-            <div className="radio-button-box">
-                <RadioButtonComponent />
-            </div>
-            <div>
-                <ConnectComponent />
-            </div>
-            <div>
-                <CheckboxComponent />
-            </div>
-            <div>
-                <TopicSelectionComponent />
-            </div>
-            <div>
-                <MentorTypeComponent />
-            </div>
-        <div>
-            <ProfileComponent/>
+          </div>
         </div>
-        <div>
-            <CareerOptionsComponent/>
+      )}
+
+      {activeLogin === 2 && !showRadioButton && (
+        <div className="second-login-box">
+          <div className="login-fields">
+            {!mentorLogin && !gmailLogin && (
+              <>
+                <button className="login-field" onClick={handleMentorLoginClick}>
+                  Login as a Mentor
+                </button>
+                <button className="login-field" onClick={handleGmailLoginClick}>
+                  Login with Gmail
+                </button>
+              </>
+            )}
+
+            {mentorLogin && (
+              <form onSubmit={handleLoginSubmit}>
+                <input type="text" placeholder="Mentor ID" required />
+                <input type="password" placeholder="Password" required />
+                <button type="submit">Submit</button>
+              </form>
+            )}
+
+            {gmailLogin && (
+              <form onSubmit={handleLoginSubmit}>
+                <input type="text" placeholder="Enter Gmail ID" required />
+                <input type="password" placeholder="Password" required />
+                <button type="submit">Submit</button>
+              </form>
+            )}
+          </div>
         </div>
-        <div>
-            <MentoringCall/>
+      )}
+
+      {showRadioButton && (
+        <div className="radio-button-box">
+          <RadioButtonComponent />
         </div>
-        <div>
-            <ReviewComponent/>
+      )}
+
+      {(activeLogin !== null || showRadioButton) && (
+        <div className="arrow-mark" onClick={handleGoBack}>
+          <FontAwesomeIcon icon={faArrowLeft} />
         </div>
-        </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/components/RadioButtonComponent.jsx
+++ src/components/RadioButtonComponent.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import './LoginPage.css';
+import CheckboxComponent from './CheckboxComponent';
 
 const RadioButtonComponent = () => {
   const [selectedOptions, setSelectedOptions] = useState([]);
+  const [showCheckBox, setShowCheckBox] = useState(false);
 
   const handleOptionChange = (event) => {
     const optionValue = event.target.value;
@@ -12,39 +15,52 @@ const RadioButtonComponent = () => {
     }
   };
 
+  const handleContinue = () => {
+    setShowCheckBox(true);
+  };
+
   return (
-    <div className="select-button-box">
-      <h2>Pick the area you want to mentor in</h2>
-      <div className="option-container">
-        <label>
-          <input
-            type="checkbox"
-            value="Data Science"
-            checked={selectedOptions.includes('Data Science')}
-            onChange={handleOptionChange}
-          />
-          Data Science
-        </label><br></br>
-        <label>
-          <input
-            type="checkbox"
-            value="Data Engineering"
-            checked={selectedOptions.includes('Data Engineering')}
-            onChange={handleOptionChange}
-          />
-          Data Engineering
-        </label><br></br>
-        <label>
-          <input
-            type="checkbox"
-            value="Product Management"
-            checked={selectedOptions.includes('Product Management')}
-            onChange={handleOptionChange}
-          />
-          Product Management
-        </label>
-      </div>
-    </div>
+    <>
+      {showCheckBox ? (
+        <CheckboxComponent />
+      ) : (
+        <div className="select-button-box">
+          <h2>Pick the area you want to mentor in</h2>
+          <div className="option-container">
+            <label>
+              <input
+                type="checkbox"
+                value="Data Science"
+                checked={selectedOptions.includes('Data Science')}
+                onChange={handleOptionChange}
+              />
+              Data Science
+            </label><br />
+            <label>
+              <input
+                type="checkbox"
+                value="Data Engineering"
+                checked={selectedOptions.includes('Data Engineering')}
+                onChange={handleOptionChange}
+              />
+              Data Engineering
+            </label><br />
+            <label>
+              <input
+                type="checkbox"
+                value="Product Management"
+                checked={selectedOptions.includes('Product Management')}
+                onChange={handleOptionChange}
+              />
+              Product Management
+            </label>
+          </div>
+          <button className="continue-button" onClick={handleContinue}>
+            Continue
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
